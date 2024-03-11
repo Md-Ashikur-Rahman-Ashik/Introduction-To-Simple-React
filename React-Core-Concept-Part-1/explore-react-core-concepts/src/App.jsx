9ca385939f5e74d712835b62120290f89347c805
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-03-11 11:19:54 +0600

    Creating function and learning about props

--- React-Core-Concept-Part-1/explore-react-core-concepts/src/App.jsx
+++ React-Core-Concept-Part-1/explore-react-core-concepts/src/App.jsx
@@ -7,14 +7,67 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
+      <Device name="Laptop" price="55"></Device>
+      <Device name="Mobile" price="17"></Device>
+      <Device name="Watch" price="3"></Device>
       <Person></Person>
+      <Student grade="7" score="99"></Student>
+      <Student grade={12} score={85}></Student>
+      <Student></Student>
+      <Developer></Developer>
     </>
   );
 }
 
-function Person(){
+function Device(props) {
+  // console.log(props);
+  return (
+    <h2>
+      This Device: {props.name}, Price: {props.price}
+    </h2>
+  );
+}
+
+function Person() {
   const age = 25;
-  return <h3>I am a person with age: {age}</h3>
+  const money = 20;
+  const person = {
+    name: "Sakib",
+    age: 12,
+  };
+  return (
+    <h3>
+      I am {person.name} with age: {age + money}
+    </h3>
+  );
+}
+
+const { grade, score } = { grade: "7", score: "99" };
+
+function Student({ grade = 1, score = 0 }) {
+  console.log(grade, score);
+  return (
+    <div className="student">
+      <h3>This is a student.</h3>
+      <p>Grade: {grade}</p>
+      <p>Score: {score}</p>
+    </div>
+  );
+}
+
+function Developer() {
+  const developerStyle = {
+    margin: "20px",
+    padding: "20px",
+    border: "2px solid purple",
+    borderRadius: "10px",
+  };
+  return (
+    <div style={developerStyle}>
+      <h5>Developer</h5>
+      <p>Coding: </p>
+    </div>
+  );
 }
 
 export default App;
