7f6ddc49bb98f343740d15d351715cb51e3aafc6
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-03-12 13:57:51 +0600

    Learning how to load dynamic data using react

--- React-Core-Concept-Part-2/react-core-concept-part-two/src/App.jsx
+++ React-Core-Concept-Part-2/react-core-concept-part-two/src/App.jsx
@@ -1,23 +1,33 @@
 import "./App.css";
 import Counter from "./Counter";
+import Friends from "./Friends";
+import Team from "./Team";
+import Users from "./Users";
+// import "./Friends.css";
 
 function App() {
-
-  function handleClick(){
+  function handleClick() {
     alert("Button Clicked");
   }
 
   const clickSecondVersion = () => {
     alert("Second Version of Button Clicked");
-  }
+  };
 
   const addToFive = (num) => {
     alert(num + 5);
-  }
+  };
 
   return (
     <>
       <h3>React Core Concepts ( Part 2 )</h3>
+
+      <Friends></Friends>
+
+      <Users></Users>
+
+      <Team></Team>
+
       <Counter></Counter>
       <button onClick={handleClick}>Click Me</button>
       <button onClick={clickSecondVersion}>Click Version Two</button>
