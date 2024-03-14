5dc25ad13f78d4d4d710c3b310f3a1e1d3489655
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-03-14 10:50:58 +0600

    Turned off prop type eslint warning

--- Simple-React-Rest-Countries/src/Components/Countries/Countries.jsx
+++ Simple-React-Rest-Countries/src/Components/Countries/Countries.jsx
@@ -2,25 +2,27 @@ import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
 // import Country from "../Country/Country";
+import "./Countries.css";
 
 const Countries = () => {
+  const [countries, setCountries] = useState([]);
 
-    const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
 
-    useEffect(() => {
-        fetch("https://restcountries.com/v3.1/all")
-        .then(res => res.json())
-        .then(data => setCountries(data))
-    }, []);
-
-    return (
-        <div>
-            <h3>Countries: {countries.length}</h3>
-            {
-                countries.map(country => <Country key={country.cca3} country={country}></Country>)
-            }
-        </div>
-    );
+  return (
+    <div>
+      <h3>Countries: {countries.length}</h3>
+      <div className="countryContainer">
+        {countries.map((country) => (
+          <Country key={country.cca3} country={country}></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
