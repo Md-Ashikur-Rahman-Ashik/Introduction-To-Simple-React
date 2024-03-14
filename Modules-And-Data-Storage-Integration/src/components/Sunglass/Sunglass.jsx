2d3f05b7874a3a296e682f0cd1eb7fdd3521ff1c
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-03-14 17:42:03 +0600

    Creating and loading fake data

---
+++ Modules-And-Data-Storage-Integration/src/components/Sunglass/Sunglass.jsx
@@ -0,0 +1,12 @@
+import Watch from "../Watch/Watch";
+import "./Sunglass.css"
+
+const Sunglass = () => {
+    return (
+        <div>
+            <Watch></Watch>
+        </div>
+    );
+};
+
+export default Sunglass;
---
+++ Modules-And-Data-Storage-Integration/src/components/Watch/Watch.jsx
@@ -0,0 +1,9 @@
+const Watch = () => {
+    return (
+        <div>
+            <h2>Watches: </h2>
+        </div>
+    );
+};
+
+export default Watch;
