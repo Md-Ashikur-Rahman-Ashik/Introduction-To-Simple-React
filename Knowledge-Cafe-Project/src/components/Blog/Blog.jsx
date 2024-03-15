0f0b4929c864152159f9d3545246e5bb3fa71219
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-03-15 17:45:40 +0600

    Displaying blog data

--- Knowledge-Cafe-Project/src/components/Blog/Blog.jsx
+++ Knowledge-Cafe-Project/src/components/Blog/Blog.jsx
@@ -1,10 +1,17 @@
 import PropTypes from 'prop-types'; // ES6
 
 const Blog = ({blog}) => {
-    console.log(blog);
+    // console.log(blog);
+
+    const {title, cover_img} = blog;
+
     return (
         <div>
-            
+            <img src={cover_img} alt={`Cover picture of ${title}`} />
+            <div>
+                
+            </div>
+            <h2 className="text-4xl">{title}</h2>
         </div>
     );
 };
--- Knowledge-Cafe-Project/src/components/Blogs/Blogs.jsx
+++ Knowledge-Cafe-Project/src/components/Blogs/Blogs.jsx
@@ -1,21 +1,24 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
+  const [blogs, setBlogs] = useState([]);
 
-    const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    fetch("blogs.json")
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  }, []);
 
-    useEffect(() => {
-        fetch("blogs.json")
-        .then(res => res.json())
-        .then(data => setBlogs(data))
-    }, [])
-
-    return (
-        <div className="w-2/3">
-            <h1 className="text-4xl">Blogs: {blogs.length}</h1>
-        </div>
-    );
+  return (
+    <div className="w-2/3">
+      <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog}></Blog>
+      ))}
+    </div>
+  );
 };
 
 export default Blogs;
